3302c6f9b340808a0f83dd80a475e769d0e9bec8
Dave Wasmer <davewasmer@gmail.com>
2023-10-30 10:03:27 +0100

    feat: initial implementation

--- Readme.md
+++ Readme.md
@@ -1,6 +1,21 @@
-# node-library-boilerplate
+# @ministry/id
 
-Basic setup for a Typescript Node.js (or runtime agnostic) library.
+Common unique ID utilities:
+
+```ts
+import { id, IdSchema, type Id } from "@ministry/id";
+
+// Generate ids
+id(); // "pfh0haxfpzowht3oi213cqos"
+id("user"); // "user_pfh0haxfpzowht3oi213cqos"
+
+// Validate ids
+IdSchema().parse("any random string will work");
+IdSchema("user").parse("user_ ensure string starts with supplied prefix");
+
+// Opaque type
+id() satisfies Id;
+```
 
 ## Usage
 
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-console.log("hello world");
+import { init } from "@paralleldrive/cuid2";
+import { z } from "zod";
+import { kebabCase } from "change-case";
+
+const createId = init({
+  length: 24,
+});
+
+export function id(prefix?: string): Id {
+  return (prefix ? kebabCase(prefix) + "_" + createId() : createId()) as Id;
+}
+
+class Brand<T extends string> {
+  private __brand!: T;
+}
+
+export type Id = string & Brand<"Id">;
+
+export const IdSchema: (prefix?: string) => z.Schema<Id> = (prefix) =>
+  (prefix
+    ? z.string().refine((id) => id.startsWith(prefix + "_"))
+    : z.string()) as unknown as z.Schema<Id>;
